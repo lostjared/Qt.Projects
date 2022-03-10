c15f2f563b14a15d99293a609dd597047381f1a7
Jared Bruni <jaredbruni@gmail.com>
2022-03-09 17:05:34 -0800

    mouse press

---
+++ source/desktop/007.mousedown/main_window.cpp
@@ -0,0 +1,28 @@
+#include"main_window.hpp"
+#include<QPainter>
+#include<QMessageBox>
+
+MainWindow::MainWindow() {
+    setWindowTitle("Grid");
+    setFixedSize(800, 600);
+}
+
+void MainWindow::paintEvent(QPaintEvent *e) {
+    Q_UNUSED(e);
+    QPainter paint(this);
+    paint.fillRect(QRect(0, 0, 800, 600), QBrush("#FFFFFF"));
+    for(int x = 0; x < 800; x += 16) {
+        for(int y = 0; y < 600; y += 16) {
+            paint.fillRect(QRect(x+1, y+1, 15, 15), QBrush("#000000"));
+        }
+    }
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *ke) {
+    QString value;
+    QTextStream stream(&value);
+    
+    stream << "Mouse pressed. Button: " << ke->button() << " x: " << ke->pos().x() << " y: " << ke->pos().y() << "\n";
+    
+    QMessageBox::information(this, value, value);
+}
---
+++ source/desktop/007.mousedown/main_window.hpp
@@ -0,0 +1,17 @@
+#ifndef __MAIN_WINDOW_H__
+#define __MAIN_WINDOW_H__
+
+#include<QMainWindow>
+#include<QMouseEvent>
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    MainWindow();
+    void paintEvent(QPaintEvent *painter);
+    void mousePressEvent(QMouseEvent *ke);
+};
+
+
+#endif
+
