55133cf4bd136b35687648caac396a26189d208d
Jared Bruni <jaredbruni@gmail.com>
2022-03-10 16:27:21 -0800

    Updated

--- source/desktop/007.mousedown/main_window.hpp
+++ source/desktop/007.mousedown/main_window.hpp
@@ -7,7 +7,7 @@
 class MainWindow : public QMainWindow {
     Q_OBJECT
 public:
-    MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = 0);
     void paintEvent(QPaintEvent *painter);
     void mousePressEvent(QMouseEvent *ke);
 };
