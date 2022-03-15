af3a6941c82999ad9135989f68676045e8b2af62
Jared Bruni <jaredbruni@gmail.com>
2022-03-14 19:26:26 -0700

    added updated how to play

--- source/desktop/games/PuzzleDrop/main_window.cpp
+++ source/desktop/games/PuzzleDrop/main_window.cpp
@@ -273,7 +273,7 @@ void GameWindow::showAbout() {
 
 void GameWindow::showHowTo() {
     QMessageBox box;
-    box.setText(tr("You play by moving the blocks left and right with the arrow keys. You press A and S to shift the blocks, Z and X to rotate them Line up the pattern of the same color 1 2 3, or 3 2 1 Either horizontal, vertical, or diagonal line Clear as many lines as you can."));
+    box.setText(tr("You play by moving the blocks left and right with the arrow keys. You press A and S to shift the blocks, Z and X to rotate them Line up the pattern of the same color 1 2 3, or 3 2 1 Either horizontal, vertical, or diagonal line. There is a wildcard block that flashes different colors, it will match any block. Clear as many lines as you can."));
     QPixmap pix = QPixmap(":/img/blue3.png");
     box.setIconPixmap(pix.scaled(QSize(64, 64)));
     box.setWindowTitle(tr("How to Play"));
