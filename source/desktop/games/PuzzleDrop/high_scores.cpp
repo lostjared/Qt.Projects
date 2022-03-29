f1d9ec03198971de16c5212fc315babaec1fca66
Jared Bruni <jaredbruni@gmail.com>
2022-03-28 18:19:39 -0700

    save/load scores from settings file

--- source/desktop/games/PuzzleDrop/high_scores.cpp
+++ source/desktop/games/PuzzleDrop/high_scores.cpp
@@ -1,14 +1,82 @@
 #include "high_scores.hpp"
 #include<QPainter>
+#include<QTextStream>
+#include<string>
 
+bool Score::operator<(const Score &s) const {
+    if(s.lines < lines) return true;
+    return false;
+}
 
-HighScores::HighScores(QWidget *parent) : QDialog(parent) {
+HighScores::HighScores(QWidget *parent) : QDialog(parent), settings("LostSideDead", "PuzzleDrop") {
     setFixedSize(640, 480);
     setWindowTitle("High Scores Window");
+    loadScores();
+}
+
+HighScores::~HighScores() {
+    saveScores();
 }
 
 void HighScores::paintEvent(QPaintEvent *e) {
     Q_UNUSED(e);
     QPainter paint(this);
     paint.fillRect(0, 0, 640, 480, QBrush("#000000"));
+    QFont font = paint.font();
+    QPen pen = paint.pen();
+    pen.setColor(QColor(QRgb(0xFF0000)));
+    font.setPixelSize(40);
+    font.setBold(true);
+    paint.setFont(font);
+    paint.setPen(pen);
+    paint.drawText(50, 50, "High Scores");
+    pen.setColor(QColor(QRgb(0xFFFFFF)));
+    font.setPixelSize(30);
+    font.setBold(true);
+    paint.setPen(pen);
+
+    font.setUnderline(true);
+    paint.setFont(font);
+    paint.drawText(50, 100, "Rank\t\tLines\t\tName");
+    font.setUnderline(false);
+    paint.setFont(font);
+
+    int pos = 150;
+
+    for(int i = 0; i < 10; ++i) {
+        QString value;
+        QTextStream stream(&value);
+        stream << (i+1) << "\t\t" << scores[i].lines << "\t\t" << scores[i].name;
+        paint.drawText(50,pos, value);
+        pos += 35;
+    }
+}
+
+void HighScores::loadScores() {
+    for(int i = 0; i < 10; ++i) {
+        QString key1, value1;
+        QTextStream stream1(&key1), stream2(&value1);
+        stream1 << i;
+        stream2 << "Anonymous:" << i*5;
+        QString score_val = settings.value(key1, value1).toString();
+        std::string value = score_val.toStdString();
+        auto pos = value.find(":");
+        std::string left = value.substr(0, pos);
+        std::string right = value.substr(pos+1, value.length());
+        Score score;
+        score.name = left.c_str();
+        score.lines = atoi(right.c_str());
+        scores.push_back(score);
+    }
+    std::sort(scores.begin(), scores.end());
+}
+
+void HighScores::saveScores() {
+    for(int i = 0; i < 10; ++i) {
+        QString key1, value1;
+        QTextStream stream1(&key1), stream2(&value1);
+        stream1 << i;
+        stream2 << scores[i].name << ":" << scores[i].lines;
+        settings.setValue(key1, value1);
+    }
 }
--- source/desktop/games/PuzzleDrop/high_scores.hpp
+++ source/desktop/games/PuzzleDrop/high_scores.hpp
@@ -2,13 +2,30 @@
 #define _PUZZLE_SCORE_H_
 
 #include<QDialog>
+#include<QSettings>
+
+
+struct Score {
+    QString name;
+    int lines;
+    bool operator<(const Score &s) const;
+
+};
+
+
 
 
 class HighScores : public QDialog {
 Q_OBJECT
 public:
     explicit HighScores(QWidget *parent = 0);
+    ~HighScores();
     void paintEvent(QPaintEvent *e);
+    void loadScores();
+    void saveScores();
+private:
+    std::vector<Score> scores;
+    QSettings settings;
 };
 
 
