ff896a0e76919a361a4b838b669c8f0eb800efc2
Jared Bruni <jaredbruni@gmail.com>
2022-04-03 15:14:36 -0700

    image example

---
+++ source/desktop/017.image/main_window.cpp
@@ -0,0 +1,29 @@
+#include"main_window.hpp"
+#include<QPainter>
+#include<QMessageBox>
+#include<QTextStream>
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
+    setWindowTitle("Click the Window");
+    setFixedSize(800, 600);
+    background = QImage(":/img/level1.png").scaled(800, 600);
+}
+
+void MainWindow::paintEvent(QPaintEvent *e) {
+    Q_UNUSED(e);
+    QPainter paint(this);
+    paint.fillRect(QRect(0, 0, 800, 600), QBrush("#000000"));
+    paint.drawImage(0, 0, background);
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *ke) {
+    QString value;
+    QTextStream stream(&value);
+    stream << "Mouse pressed. Button: " << ke->button() << " x: " << ke->pos().x() << " y: " << ke->pos().y() << "\n";    
+    QMessageBox box;
+    box.setText(value);
+    QPixmap pix = QPixmap(":/img/red1.png");
+    box.setIconPixmap(pix.scaled(QSize(64, 64)));
+    box.setWindowIcon(pix.scaled(32, 32));
+    box.setWindowTitle(tr("About Example"));
+    box.exec();
+}
