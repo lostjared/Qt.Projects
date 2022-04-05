359a261f436b3efa090689d41acbb312b463f97c
Jared Bruni <jaredbruni@gmail.com>
2022-04-05 16:23:27 -0700

    updated

--- source/desktop/019.widget/main_window.cpp
+++ source/desktop/019.widget/main_window.cpp
@@ -9,17 +9,26 @@
      label->setGeometry(15, 15, 100, 25);
      input_box = new QLineEdit("", this);
      input_box->setGeometry(125, 15, 100, 25);
+     button = new QPushButton(tr("Ok"), this);
+     button->setGeometry(250,15,50,25);
+
+     connect(button, SIGNAL(clicked()), this, SLOT(onClick()));
  }
 
  QString InputBox::text() {
      return input_box->text();
  }
 
+ void InputBox::onClick() {
+     QMessageBox::information(this,  "Data", text());
+
+ }
+
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     setWindowTitle("Enter text then click on window");
     setFixedSize(800, 600);
     box = new InputBox("Name", this);
-    box->setGeometry(15, 15, 300, 50);
+    box->setGeometry(15, 15, 350, 50);
 }
 void MainWindow::mousePressEvent(QMouseEvent *ke) {
     QString value;
--- source/desktop/019.widget/main_window.hpp
+++ source/desktop/019.widget/main_window.hpp
@@ -6,15 +6,19 @@
 #include<QLabel>
 #include<QLineEdit>
 #include<QWidget>
+#include<QPushButton>
 
 class InputBox : public QWidget {
     Q_OBJECT
 public:
     explicit InputBox(const QString &text, QWidget *parent = 0);
     QString text();
+public slots:
+    void onClick();
 private:
     QLabel *label;
     QLineEdit *input_box;
+    QPushButton *button;
 };
 
 class MainWindow : public QMainWindow {
